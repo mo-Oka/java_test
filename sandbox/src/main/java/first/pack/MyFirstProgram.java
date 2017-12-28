24570c05c313515a82c97d41ffd0101b0628a177
mo3aika@bk.ru <Lock12Lock>
2017-12-27 21:10:00 -0500

    change Rectangle and Square functions to methods

--- sandbox/src/main/java/first/pack/MyFirstProgram.java
+++ sandbox/src/main/java/first/pack/MyFirstProgram.java
@@ -8,22 +8,18 @@ public class MyFirstProgram {
     hello("Alexei");
 
     Square s = new Square(5);
-    System.out.println("Square with side length " + s.l + " = " + area(s));
+    System.out.println("Square with side length " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Square with side length " + r.a + " and " + r.b + " = " + area(r));
+    System.out.println("Square with side length " + r.a + " and " + r.b + " = " + r.area());
   }
 
   public static void hello(String somebody){
     System.out.println("Hello," + somebody + "!");
   }
 
-  public static double area(Square s) {
-    return s.l*s.l;
-  }
 
-  public static double area (Rectangle r) {
-    return r.a*r.b;
-  }
+
+
 
 }
