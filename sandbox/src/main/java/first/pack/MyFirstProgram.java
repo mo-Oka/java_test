795110db9c93551ec78a70d669b4b2eb2a2c78ba
mo3aika@bk.ru <Lock12Lock>
2017-12-28 23:46:37 -0500

    modified function distance()to method for Point object

--- sandbox/src/main/java/first/pack/MyFirstProgram.java
+++ sandbox/src/main/java/first/pack/MyFirstProgram.java
@@ -13,24 +13,12 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4,6);
     System.out.println("Square with side length " + r.a + " and " + r.b + " = " + r.area());
 
-    Point p1 = new Point();
-    p1.x = -2.3;
-    p1.y = 4;
-
-    Point p2 = new Point();
-    p2.x = 0;
-    p2.y = 8.5;
-    System.out.println(distance(p1, p2));
-
+    Point p1 = new Point(-2.3,4);
+    Point p2 = new Point(0, 8.5);
+    System.out.println("Distance between p1 and p2 = " + p1.distance(p2));
   }
 
   public static void hello(String somebody){
     System.out.println("Hello," + somebody + "!");
   }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
-  }
-
-
 }
--- sandbox/src/main/java/first/pack/Square.java
+++ sandbox/src/main/java/first/pack/Square.java
@@ -2,6 +2,7 @@ package first.pack;
 
 public class Square {
   public double l;
+
   public Square(double l){
     this.l = l;
   }
