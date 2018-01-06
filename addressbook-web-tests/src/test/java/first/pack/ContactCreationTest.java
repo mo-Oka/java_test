dd528e30740ea34cfb1f8b5733a036c9f3f6877b
mo3aika@bk.ru <Lock12Lock>
2018-01-05 21:18:26 -0500

    refactor ContactCreationTest, create ContactData object

--- addressbook-web-tests/src/test/java/first/pack/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/first/pack/ContactCreationTest.java
@@ -11,83 +11,95 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
 public class ContactCreationTest {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
+  FirefoxDriver wd;
 
-    private void login(String name, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(name);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
 
-    @Test
-    public void testContactCreation() {
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("First");
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Last");
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("nick");
-        wd.findElement(By.name("title")).click();
-        wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys("senior");
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys("company1");
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("some address in a middle of nowhere");
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("1234567890");
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("first.last@email.em");
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
-        }
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys("1990");
-        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
-        }
-        wd.findElement(By.name("notes")).click();
-        wd.findElement(By.name("notes")).clear();
-        wd.findElement(By.name("notes")).sendKeys("test note");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String name, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(name);
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+  public void testContactCreation() {
+    initContactCreation(By.linkText("add new"));
+    fillContactForm(new ContactData("First", "Last", "nick", "senior", "company1", "some address in a middle of nowhere", "1234567890", "first.last@email.em", "1990", "test note"));
+    submitContactForm();
+  }
+
+  private void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompanyName());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option[3]")).click();
     }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option[3]")).click();
     }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(contactData.getYear());
+    if (!wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).isSelected()) {
+      wd.findElement(By.xpath("//div[@id='content']/form/select[5]//option[2]")).click();
     }
+    wd.findElement(By.name("notes")).click();
+    wd.findElement(By.name("notes")).clear();
+    wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+  }
+
+  private void submitContactForm() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void initContactCreation(By add_new) {
+    wd.findElement(add_new).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
 }
---
+++ addressbook-web-tests/src/test/java/first/pack/ContactData.java
@@ -0,0 +1,67 @@
+package first.pack;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String nickName;
+  private final String title;
+  private final String companyName;
+  private final String address;
+  private final String homePhone;
+  private final String email;
+  private final String year;
+  private final String notes;
+
+  public ContactData(String firstName, String lastName, String nickName, String title, String companyName, String address, String homePhone, String email, String year, String notes) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.title = title;
+    this.companyName = companyName;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.email = email;
+    this.year = year;
+    this.notes = notes;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
