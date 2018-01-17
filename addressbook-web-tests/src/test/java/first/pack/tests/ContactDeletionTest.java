8100c1be02a7d0b6d86d43c37039f7928fb56adc
mo3aika@bk.ru <Lock12Lock>
2018-01-16 22:49:59 -0500

    add goToHomePage step in all contact tests

--- addressbook-web-tests/src/test/java/first/pack/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/first/pack/tests/ContactDeletionTest.java
@@ -11,7 +11,6 @@ public class ContactDeletionTest extends TestBase{
     if (!app.getContactHelper().isThereAContact()){
       app.getContactHelper().createContact(new ContactData("First", "Last", "nick", "company1", "senior", "some address in a middle of nowhere", "1234567890", "first.last@email.em", "1", "June", "1990", "test1","test note"));
     }
-    app.getNavigationHelper().goToHomePage();
     app.getContactHelper().goToContactPage();
     app.getContactHelper().deleteSelectedContact();
     //app.getContactHelper().clickAlert();
--- addressbook-web-tests/src/test/java/first/pack/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/first/pack/tests/ContactModificationTest.java
@@ -11,7 +11,6 @@ public class ContactModificationTest extends TestBase{
     if (!app.getContactHelper().isThereAContact()){
       app.getContactHelper().createContact(new ContactData("First", "Last", "nick", "company1", "senior", "some address in a middle of nowhere", "1234567890", "first.last@email.em", "1", "June", "1990", "test1","test note"));
     }
-    app.getNavigationHelper().goToHomePage();
     app.getContactHelper().goToContactPage();
     app.getContactHelper().fillContactForm(new ContactData("Name1-2","Last1-2","nick1-2","company1-2","title1-2","address1-2","123","qw@qw.qw","2","December","2000",null,"note1-2"), false);
     app.getContactHelper().submitContactModification();
